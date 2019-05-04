dad6273cfe28f8d7b02bd4b428f516b1c35151a6
sumancvb <sumancvb@gmail.com>
2019-05-04 19:49:30 +0530

    Update uninstall-deluge-ubuntu.sh

--- uninstall-deluge-ubuntu.sh
+++ uninstall-deluge-ubuntu.sh
@@ -6,11 +6,11 @@ echo "*******************************************************"
 
 systemctl stop deluged
 systemctl disable deluged
-rm /etc/systemd/system/deluged
+rm /etc/systemd/system/deluged.service
 
 systemctl stop deluge-web
 systemctl disable deluge-web
-rm /etc/systemd/system/deluge-web
+rm /etc/systemd/system/deluge-web.service
 systemctl daemon-reload
 systemctl reset-failed
 
